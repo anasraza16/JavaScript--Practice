26e861f8a0e9fbda96bcc7d123d6d6170f27cbd3
Ahmed Raza <razaplus360@gmail.com>
2026-03-29 02:50:53 +0500

    js revision class 1

---
+++ Class-01/app.js
@@ -0,0 +1,17 @@
+// CONCATINATION
+
+// console.log(2 + 2 + " 2 is equals to 2 ");
+// console.log(2 is equals to 2 " + 2 + 2);
+
+// ELSE IF CONDITION
+
+if(user1) {
+    console.log("user1 founded");
+    
+}else if(user2) {
+    console.log("user 2 founded");
+    
+}else{
+    console.log("Nothing Found");
+    
+}
